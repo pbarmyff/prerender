c66227225de18f56f7e04b45d7babc28b3e72dbe
pbarmyff <94227876+pbarmyff@users.noreply.github.com>
2025-04-08 21:54:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 #!/usr/bin/env node
-const { exec } = require('child_process');
 const prerender = require('./lib');
 
-exec('node chrome.js', (err, stdout, stderr) => {
-  if (err) {
-    console.error('🚫 Failed to launch Chrome:', err);
-    return;
-  }
-
-  console.log(stdout);
-
-  const server = prerender();
-
-  server.use(prerender.sendPrerenderHeader());
-  server.use(prerender.browserForceRestart());
-  server.use(prerender.addMetaTags());
-  server.use(prerender.removeScriptTags());
-  server.use(prerender.httpHeaders());
-
-  server.start();
+const server = prerender({
+  chromeLocation: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium'
 });
+
+server.use(prerender.sendPrerenderHeader());
+server.use(prerender.browserForceRestart());
+server.use(prerender.addMetaTags());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
+
+server.start();
